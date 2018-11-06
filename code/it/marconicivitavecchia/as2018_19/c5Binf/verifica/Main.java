ea3388ffe6c113a8d3e110b8213c2c1a2b22e787
Alessio <al.orchi@gmail.com>
2018-11-06 11:40:50 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,6 +1,6 @@
 
 // TODO Inserire il package corretto
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Binf.verifica ;
 
 import javax.swing.*;
 
@@ -11,16 +11,27 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(300,200);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb=new StringBuilder();
 		
 		// TODO Creare la stringa HTML per il wireframe
-		
+		sb.append("<html>");
+	    sb.append("<div style='display:table'>");
+        sb.append("div style='display:table-row'>");
+        sb.append("<h1>Almanacco Astronomico </h>");
+        sb.append("</div");
+        sb.append("<div style='display:table-row'>");
+        sb.append("<h3> Novembre 2018 <h3>");
+        sb.append("</div>");
+        sb.append("</div>");
+		sb.append("</html>");
 		// TODO Convertire lo StringBuilder in String
-		
+		String htmlText;
+		htmlText=sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(htmlText));
 		super.setVisible(true);
 		 
 		System.out.println(htmlText);
